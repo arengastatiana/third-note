a4a4f80833afbba608b3b551b86eedb0fd4d5450
Nicolas <colombia1@gmail.com>
2024-10-08 07:29:48 -0500

    commit

---
+++ mainn.java
@@ -0,0 +1,14 @@
+public class mainn {
+    public static void main (String{} args);{
+
+    int edad = 19;
+    double altura = 1.64; 
+    char inicialNombre = "T";
+    boolean mayorEdad = edad >=19;
+
+    System.out.println("mi edad es:" + edad);
+    System.out.println("mi edad es:" + edad);
+    System.out.println("mi edad es:" + edad);
+    System.out.println("mi edad es:" + edad);
+}
+}
