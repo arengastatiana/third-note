e072fb4b06c12914966a451a08c94c67d08d213c
Nicolas <colombia1@gmail.com>
2024-10-08 07:13:21 -0500

    commit

---
+++ Ejercicioo.java
@@ -0,0 +1,15 @@
+public class Ejercicioo {
+    public static void (String{} args){
+        System.out.println("bienvenido a koaj");
+        byte Tshirt = 25;
+        byte pants = 30;
+        double sale15 = 0.85;
+        double sale5 = 0.95;
+
+        double salepants = pants * sale15;
+        double saletshirt = Tshirt * sales15;
+
+        double totalsale = salepants + saletshirt;
+        System.out.println("");
+    }
+}
